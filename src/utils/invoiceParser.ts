c369b06217d183b5b3b090d252f644344d298d96
iamjashitwbu <jashitkale1234@gmail.com>
2026-04-01 16:51:06 +0530

    Update invoiceParser.ts

--- src/utils/invoiceParser.ts
+++ src/utils/invoiceParser.ts
@@ -1,7 +1,7 @@
 import * as pdfjsLib from 'pdfjs-dist';
 import { InvoiceData } from '../types';
 
-pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.10.38/pdf.worker.min.js';
 
 async function convertPdfToJpegBase64(file: File): Promise<string> {
   const arrayBuffer = await file.arrayBuffer();
